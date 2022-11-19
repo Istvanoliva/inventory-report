249e7d8770e30ffeba5fd860dd3a5d95b1718cbf
Istvan <olivaistvan@gmail.com>
2022-11-19 10:06:55 -0300

    tests: (product) cria_produto()

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
-
+from inventory_report.inventory.product import Product
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, 'Chocolate', 'Sol', '10/10/22', '10/10/23', 12345, 'em local fresco'
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'Chocolate'
+    assert product.nome_da_empresa == 'Sol'
+    assert product.data_de_fabricacao == '10/10/22'
+    assert product.data_de_validade == '10/10/23'
+    assert product.numero_de_serie == 12345
+    assert product.instrucoes_de_armazenamento == 'em local fresco'
