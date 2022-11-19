abf287e3629da7ff1cd1d8b441318e0258d5ad98
Istvan <olivaistvan@gmail.com>
2022-11-19 10:18:05 -0300

    fix: flake8 errors

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -3,13 +3,13 @@ from inventory_report.inventory.product import Product
 
 def test_relatorio_produto():
     product = Product(
-        1, 'Chocolate', 'Sol', '10-10-22', '10-10-23', 12345, 'em local fresco'
+        1, 'Chocolate', 'Sol', '10/10/22', '10/10/23', 12345, 'em local fresco'
     )
 
     assert (
         str(product) == "O produto Chocolate"
-        " fabricado em 10-10-22"
+        " fabricado em 10/10/22"
         " por Sol com validade"
-        " até 10-10-23"
+        " até 10/10/23"
         " precisa ser armazenado em local fresco."
     )
