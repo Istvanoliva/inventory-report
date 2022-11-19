2f372dabe51571b1c2f2aa1f91d760e5022441f2
Istvan <olivaistvan@gmail.com>
2022-11-19 10:10:08 -0300

    tests: (product_report) relatorio_produto()

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, 'Chocolate', 'Sol', '10-10-22', '10-10-23', 12345, 'em local fresco'
+    )
+
+    assert (
+        str(product) == "O produto Chocolate"
+        " fabricado em 10-10-22"
+        " por Sol com validade"
+        " até 10-10-23"
+        " precisa ser armazenado em local fresco."
+    )
