6f610879bad23e4bbbee59255fd3e6ee87fdc5aa
Istvan <olivaistvan@gmail.com>
2022-11-24 21:47:18 -0300

    feat: (inventory) add read_json()

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -8,14 +9,25 @@ class Inventory:
 
     @classmethod
     def import_data(cls, path, type):
-        products = cls.read_csv(path)
-        
+        products = []
+
+        if path.endswith('csv'):
+            products = cls.read_csv(path)
+        if path.endswith('json'):
+            products = cls.read_json(path)
+
         if (type == 'simples'):
             return SimpleReport.generate(products)
-        return CompleteReport.generate(products)
+        else:    
+            return CompleteReport.generate(products)
 
 
     @classmethod
     def read_csv(cls, path):
         with open(path) as csv_file:
             return list(csv.DictReader(csv_file))
+
+    @classmethod
+    def read_json(cls, path):
+        with open(path) as json_file:
+            return json.loads(json_file.read())
