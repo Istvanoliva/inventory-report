2f35879a4db27aac983a8b1ab41f4915be7b38ad
Istvan <olivaistvan@gmail.com>
2022-11-26 04:31:31 -0300

    test: (report_decorator)

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,35 @@
-# from inventory_report.reports.colored_report import ColoredReport
+import pytest
+
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def products():
+    return [
+        {
+            "id": 1,
+            "nome_do_produto": "Chocolate",
+            "nome_da_empresa": "Sol",
+            "data_de_fabricacao": "2021-07-10",
+            "data_de_validade": "2024-07-10",
+        }
+    ]
+
+
+def test_decorar_relatorio(products):
+    red = "\033[31m"
+    green = "\033[32m"
+    blue = "\033[36m"
+    color = "\033[0m"
+
+    report = ColoredReport(SimpleReport).generate(products)
+
+    assert f"{green}Data de fabricação mais antiga:{color}" in report
+    assert f"{blue}2021-07-10{color}" in report
+
+    assert f"{green}Data de validade mais próxima:{color}" in report
+    assert f"{blue}2024-07-10{color}" in report
+
+    assert f"{green}Empresa com mais produtos:{color}" in report
+    assert f"{red}Sol{color}" in report
