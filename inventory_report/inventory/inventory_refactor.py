86ba7a9b713c13a91f9e486747527a409d608561
Istvan <olivaistvan@gmail.com>
2022-11-28 01:23:24 -0300

    feat: create class InventoryRefactor

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer: Importer):
+        self.importer = importer
+        self.data = []
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+    def __verify_type_inventory(self, type):
+        if type == "simples":
+            return SimpleReport.generate(self.data)
+        elif type == "completo":
+            return CompleteReport.generate(self.data)
+        else:
+            raise ValueError("Tipo de relatório inválido")
+
+
+    def import_data(self, path, type):
+        self.data += self.importer.import_data(path)
+        return self.__verify_type_inventory(type)
