3a32a043216f30d98d0f1f5d1147ba7e271e34fb
Istvan <olivaistvan@gmail.com>
2022-11-28 01:24:11 -0300

    feat: implements main function

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+
+
+def get_importer_type(path):
+    if path.endswith("csv"):
+        return CsvImporter
+    if path.endswith("xml"):
+        return XmlImporter
+    if path.endswith("json"):
+        return JsonImporter
+
 def main():
-    pass
+    try:
+        path, report_type = sys.argv[1:]
+    except ValueError:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+
+    importer = get_importer_type(path)
+    inventory = InventoryRefactor(importer)
+
+    print(inventory.import_data(path, report_type), end="")
